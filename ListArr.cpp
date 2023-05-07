1ace0da8c6c655d8d9e789cb238bbe2ce39ee9b1
clcser <clcser2021@udec.cl>
2023-05-07 14:22:03 -0400

    main structure and methods skeleton

---
+++ ListArr.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "ListArr.h"
+
+ListArr::ListArr() {
+
+}
+
+ListArr::~ListArr() {
+
+}
+
+int ListArr::size() {
+
+}
+
+void ListArr::insert(int data, int index) {
+
+}
+
+void ListArr::insert_left(int data) {
+
+}
+
+void ListArr::print() {
+
+}
+
+bool ListArr::find(int data) {
+
+}
+
+void ListArr::createTree(ArrNode* first) {
+
+}
